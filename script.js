c98bc07e7ab65312fc2aa45525940622a7b49a8f
rutabageldev <187210498+rutabageldev@users.noreply.github.com>
2024-11-11 14:04:19 -0500

    Created and implemented basic buildListItem.
    Created function to accept text and build a basic list item with the text
    passthrough and a delete button. Still needs input validation and delete
    implementation.

---
+++ script.js
@@ -0,0 +1,33 @@
+const addButton = document.querySelector(".button-add");
+const inputField = document.querySelector("#text-input");
+const listContainer = document.querySelector(".list-container");
+
+addButton.addEventListener("click", addItem);
+
+function addItem(e) {
+    console.log(e);
+    const text = inputField.value.trim();
+    listContainer.appendChild(buildListContainer(text));
+}
+
+function buildListContainer(text) {
+    // Create list item container
+    const itemContainer = document.createElement("li");
+    itemContainer.classList.add("item-container");
+
+    // Create the main text div
+    const listItem = document.createElement("div")
+    listItem.classList.add("list-item");
+    listItem.textContent = text;
+
+    // Create the delete button
+    const deleteButton = document.createElement("button");
+    deleteButton.classList.add("button-delete");
+    deleteButton.textContent = "Delete";
+
+    // Build the list item
+    itemContainer.appendChild(listItem);
+    itemContainer.appendChild(deleteButton);
+
+    return itemContainer;
+}
